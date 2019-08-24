0398acef45009807e92769765cb97d6d9247f52c
B0rjitaaa <bmcx12@gmail.com>
2019-08-24 22:59:28 +0200

    Added lesson code for Mac Changer.

---
+++ 01-mac_changer/mac_changer.py
@@ -0,0 +1,50 @@
+import subprocess
+import optparse
+import re
+import os
+
+
+def check_super_user():
+    return os.geteuid()==0
+
+def get_current_mac(interface):
+    import ipdb; ipdb.set_trace()
+    ifconfig_result = subprocess.check_output(['ifconfig', interface])
+    mac_addr = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', str(ifconfig_result))
+    if mac_addr:
+        return mac_addr.group(0)
+    else:
+        print('[!] Could not read MAC address.')
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--interface', dest='interface', help='Interface to change its MAC addr')
+    parser.add_option('-m', '--mac', dest='new_mac', help='New MAC addr')
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error('[!] Please specify an interface, use --help for more info.')
+    elif not options.new_mac:
+        parser.error('[!] Please specify a MAC, use --help for more info.')
+    return options
+
+
+def change_mac(interface, new_mac):
+    print('[+] Changing MAC addres for {} to {}'.format(interface, new_mac))
+    subprocess.run(['ifconfig', interface, 'down'])
+    subprocess.run(['ifconfig', interface, 'hw', 'ether', new_mac])  # Depends on our interface.
+    subprocess.run(['ifconfig', interface, 'up'])
+
+
+if __name__ == '__main__':
+    if check_super_user():
+        options = get_arguments()
+        current_mac = get_current_mac(options.interface)
+        print(f'Current MAC: {current_mac}')
+
+        change_mac(options.interface, options.new_mac)
+
+        if current_mac == options.new_mac:
+            print(f'[+] MAC address was successfully changed to {options.new_mac}.')
+        else:
+            print('[!] MAC address did not get changed.')
